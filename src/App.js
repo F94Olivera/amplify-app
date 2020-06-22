b1d12b75dcb9ccb846cd45b1379730c9b16dbcf6
Facu Olivera <f94olivera@gmail.com>
2020-06-22 16:12:03 -0300

    mensaje de amor para Juanma <3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h6 className="App-logo"> Juanma Gato!!!!</h6>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
